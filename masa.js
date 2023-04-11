57e55892e00709caf042ced14b9502dd8bea2159
Masashi <ma.orange381@gmail.com>
2023-04-11 10:22:01 -0700

    add autocomplete city

---
+++ masa.js
@@ -0,0 +1,21 @@
+let autocomplete;
+function initAutocomplete() {
+  autocomplete = new google.maps.places.Autocomplete(
+  document.getElementById('autocomplete'),
+  {
+    types: ["establishment"],
+    componentRestrictions: {'country' : ["CA"]},
+    fields: ['place_id', 'geometry', 'name']
+  });
+}
+
+function onPlaceChanged() {
+  const place = autocomplete.getPlace();
+  if(!place.geometry) {
+    // User did not select a prediction: reset the input field
+    document.getElementById('autocomplete').Placeholder = 'Enter a place';
+  } else {
+    // Display details about the valid place
+    document.getElementById('details').interHTML = place.name;
+  }
+}
