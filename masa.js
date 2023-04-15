659203ba88f698e3128af4adc211f474cafaba64
Masashi <ma.orange381@gmail.com>
2023-04-15 14:18:09 -0700

    local strage

--- masa.js
+++ masa.js
@@ -29,35 +29,42 @@ function onPlaceChanged() {
 
 const clicked = document.getElementById('star');
 clicked.addEventListener('click', () => {
-  const select = document.getElementById('favoriteCity');
-  const addOption = document.createElement('option');
-  addOption.textContent = document.getElementById('cityName').innerText;
-  select.append(addOption);
-  //local storage
-  const key = 'city';
-  let favoriteArr = [];
-  for(let i = 1; i < select.children.length; i++){
-    favoriteArr.push(select.children[i].textContent);
-  }
-    const getJson = localStorage.getItem(key);
-    const parsedCity = JSON.parse(getJson);
-    if(parsedCity == null){
-      addCity = new Array(favoriteArr);
-      const json = JSON.stringify(favoriteArr);
-      localStorage.setItem(key, json);
-    } else{
-      if(parsedCity.indexOf(favoriteArr) == -1){
-        parsedCity.push(favoriteArr);
-        const json = JSON.stringify(favoriteArr);
-        localStorage.setItem(key, json);
-      }
-    }
   // const select = document.getElementById('favoriteCity');
   // const addOption = document.createElement('option');
-  // for(let i = 0; i < select.children.length; i++){
-  //   if(select.children[i] == null){
-  //     addOption.textContent = document.getElementById('cityName').innerText;
-  //     select.append(addOption);
-  //   }
+  // addOption.textContent = document.getElementById('cityName').innerText;
+  // select.append(addOption);
+  // //local storage
+  // const key = 'city';
+  // let favoriteArr = [];
+  // for(let i = 1; i < select.children.length; i++){
+  //   favoriteArr.push(select.children[i].textContent);
   // }
+  //   const getJson = localStorage.getItem(key);
+  //   const parsedCity = JSON.parse(getJson);
+  //   if(parsedCity == null){
+  //     addCity = new Array(favoriteArr);
+  //     const json = JSON.stringify(favoriteArr);
+  //     localStorage.setItem(key, json);
+  //   } else{
+  //     if(parsedCity.indexOf(favoriteArr) == -1){
+  //       parsedCity.push(favoriteArr);
+  //       const json = JSON.stringify(favoriteArr);
+  //       localStorage.setItem(key, json);
+  //     }
+  //   }
+  const select = document.getElementById('favoriteCity');
+  const addOption = document.createElement('option');
+  let addSelect = [];
+  for(let i = 0; i < select.children.length; i++){ 
+    addSelect.push(select.children[i].textContent);
+  }
+  addOption.textContent = document.getElementById('cityName').innerText;
+  // let newSelect = [];
+  if(addSelect.indexOf(addOption.textContent) == -1){   
+    select.append(addOption);
+    const key = 'city';
+    addSelect.push(addOption.textContent);
+    const json = JSON.stringify(addSelect);
+    localStorage.setItem(key, json);
+  }
 });
