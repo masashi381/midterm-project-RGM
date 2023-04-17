8ae971a6a5bd6186b34cc8a6461361d4066315f0
RafitaToPlay <rafael_serrano_45@hotmail.com>
2023-04-17 01:39:13 -0700

    update js and html

---
+++ rafa.js
@@ -0,0 +1,21 @@
+const url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+
+fetch (url)
+    .then (response => response.json())
+    .then (data => {
+        const hourlyData = data.list.filter(item => {
+            const date = new Date(item.dt * 1000);
+            const now = Date();
+            const hoursDiff = (date.getTime() - now.getTime()) / (1000 * 60 * 60);
+            return hoursDiff >= 0 && hoursDiff <= 24;                    
+    });
+    hourlyData.forEach((item, index) => {
+        const temp = item.main.temp;
+        const description = item.weather[0].description;
+        const element = document.querySelector(`#weather-${index} .weather-data`);
+        element.textContent = `Temp: ${temp}°C, ${description}`;
+    });
+})
+.catch(error => {
+    console.error(error);
+});
