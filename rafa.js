621ddfec74d032c9df032aff6a9e4d0f5d24c5d5
Masashi <ma.orange381@gmail.com>
2023-04-17 09:52:23 -0700

    weather API8

--- rafa.js
+++ rafa.js
@@ -1,15 +1,17 @@
-const url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apikey}"
 const latitude = "49.246292";
-const longitude = "-123.116226"
-const APIKey = "ff3523e635ffd098e66980c521a7d050"
+const longitude = "-123.116226";
+const APIKey = "ff3523e635ffd098e66980c521a7d050";
+const units = "metric";
+const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${APIKey}&units=${units}`;
 fetch (url)
     .then (response => response.json())
     .then (data => {
         const hourlyData = data.list.filter(item => {
             const date = new Date(item.dt * 1000);
-            const now = Date();
-            const hoursDiff = (date.getTime() - now.getTime()) / (1000 * 60 * 60);
-            return hoursDiff >= 0 && hoursDiff <= 24;                    
+            const now = date.getDate();
+            // const hoursDiff = (date.getTime() - now.getTime()) / (1000 * 60 * 60);
+            // return hoursDiff >= 0 && hoursDiff <= 24; 
+            return now >= 0 && now <= 24;                   
     });
     hourlyData.forEach((item, index) => {
         const temp = item.main.temp;
@@ -18,6 +20,6 @@ fetch (url)
         element.textContent = `Temp: ${temp}°C, ${description}`;
     });
 })
-.catch(error => {
-    console.error(error);
-});
+// .catch(error => {
+//     console.error(error);
+// });
